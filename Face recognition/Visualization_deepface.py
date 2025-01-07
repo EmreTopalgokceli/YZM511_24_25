49ce884261c4a6172773b017c1bc91d796675d61
Emre-Topalgokceli <94282435+EmreTopalgokceli@users.noreply.github.com>
2025-01-07 18:17:50 -0500

    Add files via upload

---
+++ Face recognition/Visualization_deepface.py
@@ -0,0 +1,25 @@
+from deepface import DeepFace
+import os
+import pandas as pd
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")  # Grafik arayüzü için uygun backend kulla
+
+
+def visualize_attribute(img_path, result, attribute="age"):
+    # Read and convert the image
+    image = cv2.imread(img_path)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Generate the display text
+    text = f"{attribute}: {result[0].get(attribute, 'not found')}"
+
+    # Display the image with the attribute text
+    plt.imshow(image_rgb)
+    plt.axis('off')
+    plt.title(text)
+    plt.show()
+
+# Example usage
+visualize_attribute(img_path, result, "age")
