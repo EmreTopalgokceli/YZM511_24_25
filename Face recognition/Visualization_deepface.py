3e2272e9a5de902988933423571d55e624b0e6f3
Emre-Topalgokceli <94282435+EmreTopalgokceli@users.noreply.github.com>
2025-01-08 09:12:44 -0500

    Update Visualization_deepface.py

--- Face recognition/Visualization_deepface.py
+++ Face recognition/Visualization_deepface.py
@@ -8,18 +8,18 @@ matplotlib.use("TkAgg")
 
 
 def visualize_attribute(img_path, result, attribute="age"):
-    # Read and convert the image
+    
     image = cv2.imread(img_path)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # Generate the display text
+    
     text = f"{attribute}: {result[0].get(attribute, 'not found')}"
 
-    # Display the image with the attribute text
+    
     plt.imshow(image_rgb)
     plt.axis('off')
     plt.title(text)
     plt.show()
 
-# Example usage
+
 visualize_attribute(img_path, result, "age")
